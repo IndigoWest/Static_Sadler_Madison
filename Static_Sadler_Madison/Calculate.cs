fda3d2fe2ba620cc06d4247574172103fa28e966
IndigoWest <maddie.sadler2005@gmail.com>
2023-04-24 10:40:16 -0500

    Add project files.

---
+++ Static_Sadler_Madison/Calculate.cs
@@ -0,0 +1,80 @@
+﻿// System namespace needed to use Console class
+using System;
+// System.Collections.Generic namespace needed to use Console class
+using System.Collections.Generic;
+// System.Text namespace needed to use Console class
+using System.Text;
+
+// My collection of classes for this project
+namespace Static_Sadler_Madison
+{
+    // Main class for Calculate.cs
+    class Calculate
+    {
+        // The Main method is executed when the application starts
+        // Initializes a public static float called result and assigns the value 0.0f
+        public static float result = 0.0f;
+
+        // Creates static Calculate()
+        static Calculate()
+        {
+            // Shows result is 0.0f
+            result = 0.0f;
+        }
+        // Adds ints x and y
+        public static int Add(int x, int y)
+        {
+            // Returns x + y
+            return x + y;
+        }
+
+        // Subtracts ints x and y
+        public static int Sub(int x, int y)
+        {
+            // Returns x - y
+            return x - y;
+        }
+
+        // Multiplies ints x and y
+        public static int Mult(int x, int y)
+        {
+            // Returns x * y
+            return x * y;
+        }
+
+        // Divides ints x and y
+        public static int Div(int x, int y)
+        {
+            // Returns x / y
+            return x / y;
+        }
+
+        // Adds floats x and y
+        public static float Add(float x, float y)
+        {
+            // Returns x + y
+            return x + y;
+        }
+
+        // Subtracts floats x and y
+        public static float Sub(float x, float y)
+        {
+            // Returns x - y
+            return x - y;
+        }
+
+        // Multiplies floats x and y
+        public static float Mult(float x, float y)
+        {
+            // Returns x * y
+            return x * y;
+        }
+
+        // Divides floats x and y
+        public static float Div(float x, float y)
+        {
+            // Returns x / y
+            return x / y;
+        }
+    }
+}
---
+++ Static_Sadler_Madison/Program.cs
@@ -0,0 +1,32 @@
+﻿// System namespace needed to use console class
+using System;
+
+// My collection of classes for this project
+namespace Static_Sadler_Madison
+{
+    // Main class for Program.cs
+    class Program
+    {
+        // The Main method is executed when the application starts
+        static void Main(string[] args)
+        {
+            // Prints addition calculation to the console and assigns 13 to x and 92 to y
+            Console.WriteLine(Calculate.Add(13, 92));
+            // Prints subtraction calculation to the console and assigns 59 to x and 21 to y
+            Console.WriteLine(Calculate.Sub(59, 21));
+            // Prints multiplication calculation to the console and assigns 2 to x and 9 to y
+            Console.WriteLine(Calculate.Mult(2, 9));
+            // Prints division calculation to the console and assigns 28 to x and 4 to y
+            Console.WriteLine(Calculate.Div(28, 4));
+
+            // Prints addition calculation to the console and assigns 28.49f to x and 29.59f to y
+            Console.WriteLine(Calculate.Add(28.49f, 29.59f));
+            // Prints subtraction calculation to the console and assigns 10.47f to x and 2.43f to y
+            Console.WriteLine(Calculate.Sub(10.47f, 2.43f));
+            // Prints multiplication calculation to the console and assigns 19.39f to x and 29.38f to y
+            Console.WriteLine(Calculate.Mult(19.39f, 29.38f));
+            // Prints division calculation to the console and assigns 92.57f to x and 29.32f to y
+            Console.WriteLine(Calculate.Div(92.57f, 29.32f));
+        }
+    }
+}
